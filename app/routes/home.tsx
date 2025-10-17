5c7d34816d731ad788de624287c43c6b9d34e6eb
veenit9560 <veenit704kumar@gmail.com>
2025-10-17 14:15:04 +0530

    implement wipe functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       <section className="main-section">
           <div className="page-heading py-16">
               <h1>Track Your Applications & Resume Ratings</h1>
-              { loadingResumes && resumes.length===0 ? (
+              { !loadingResumes && resumes.length===0 ? (
                   <h2>No resumes found. Upload your first resume to get feedback.</h2>
               ) :(
                   <h2>Review your submissions and check AI-powered feedback.</h2>
